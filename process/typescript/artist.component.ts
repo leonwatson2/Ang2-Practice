a372dd8e23863dfd6badb4317b951e29f89fd4ba
vlw0052 <leon@vlw2.com>
2016-07-01 18:24:30 -0500

    Artist Component

---
+++ process/typescript/artist.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'artist-item',
+	templateUrl: 'partials/artist-item.html',
+	inputs:['artist']
+})
+
+export class ArtistItemComponent {
+
+}
+
+
